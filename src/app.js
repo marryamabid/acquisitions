3b1bad58022cd81e71508fa2a3d897c0d7972a22
Marryam Abid <marryam.abid11@gmail.com>
2026-05-04 18:05:31 +0500

    setup logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import logger from '#config/winston.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message.trim()),
+    },
+  })
+);
 
 app.get('/', (req, res) => {
+  logger.info('hello from acqusitions');
   res.status(200).send('hello from acqusitions');
 });
 
---
+++ src/config/winston.js
@@ -0,0 +1,28 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stacke: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'acquisitions_api' },
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.simple(),
+        winston.format.colorize()
+      ),
+    })
+  );
+}
+export default logger;
