387172e87bd8a8de716c0177e2cd313133970831
Marryam Abid <marryam.abid11@gmail.com>
2026-05-04 15:54:06 +0500

    add eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello from acqusitions");
+app.get('/', (req, res) => {
+  res.status(200).send('hello from acqusitions');
 });
 
 export default app;
